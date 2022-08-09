c7f600a4da3b7283a18f5801d83860f7eea9d810
HarshCasper <erbeusgriffincasper@gmail.com>
2022-08-09 23:21:20 +0530

    implement suggestions

--- scripts/install-conda.sh
+++ scripts/install-conda.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 set -xe
 
-curl -sL -o miniconda.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
-sh ./miniconda.sh -b -p /opt/conda
-rm miniconda.sh
+# Requires environment MAMBAFORGE_SHA256, MINIFORGE_VERSION, and DEFAULT_ENV
+wget --quiet -O mambaforge.sh https://github.com/conda-forge/miniforge/releases/download/$MAMBAFORGE_VERSION/Mambaforge-Linux-x86_64.sh
+echo "${MAMBAFORGE_SHA256} mambaforge.sh" > mambaforge.checksum
+
+echo $(sha256sum -c mambaforge.checksum)
+
+if [ $(sha256sum -c mambaforge.checksum | awk '{print $2}') != "OK" ]; then
+   echo Error when testing checksum
+   exit 1;
+fi
+
+# Install Mamba and clean-up
+sh ./mambaforge.sh -b -p /opt/conda
+rm mambaforge.sh mambaforge.checksum
+
+mamba --version
+mamba clean -afy
+
+ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
 
 mkdir -p /etc/conda
 cat <<EOF > /etc/conda/condarc
