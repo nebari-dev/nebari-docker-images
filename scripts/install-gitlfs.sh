f9e539d8154b3144c711f224fe990985d5db2a56
viniciusdc <vinivdc2009@hotmail.com>
2024-01-18 13:01:53 -0300

    test basic dep. inclusion

--- scripts/install-gitlfs.sh
+++ scripts/install-gitlfs.sh
@@ -1,20 +1,20 @@
-#!/usr/bin/env bash
-# Copyright (c) Nebari Development Team.
-# Distributed under the terms of the Modified BSD License.
+# #!/usr/bin/env bash
+# # Copyright (c) Nebari Development Team.
+# # Distributed under the terms of the Modified BSD License.
 
-set -xe
+# set -xe
 
-# Adding the packagecloud repository for git-lfs installation
-wget --quiet https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh
-expected_sum=5fc673f9a72b94c011b13eb5caedc3aa4541b5c5506b95d013cb7ba0f1cf66cf
+# # Adding the packagecloud repository for git-lfs installation
+# wget --quiet https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh
+# expected_sum=5fc673f9a72b94c011b13eb5caedc3aa4541b5c5506b95d013cb7ba0f1cf66cf
 
-if [[ ! $(sha256sum script.deb.sh) == "${expected_sum}  script.deb.sh" ]]; then
-    echo Unexpected hash from git-lfs install script
-    exit 1
-fi
+# if [[ ! $(sha256sum script.deb.sh) == "${expected_sum}  script.deb.sh" ]]; then
+#     echo Unexpected hash from git-lfs install script
+#     exit 1
+# fi
 
-# Install packagecloud's repository signing key and add repository to apt
-sh ./script.deb.sh
+# # Install packagecloud's repository signing key and add repository to apt
+# sh ./script.deb.sh
 
-# Install git-lfs
-apt-get install -y --no-install-recommends git-lfs
+# # Install git-lfs
+# apt-get install -y --no-install-recommends git-lfs
