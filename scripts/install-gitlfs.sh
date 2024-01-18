403d9d7a5de7e60967b8de5ef9c0ae4438337342
viniciusdc <vinivdc2009@hotmail.com>
2024-01-18 12:44:22 -0300

    Include git-lfs as part of base install

---
+++ scripts/install-gitlfs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Copyright (c) Nebari Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+# Adding the packagecloud repository for git-lfs installation
+wget --quiet https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh
+expected_sum=5fc673f9a72b94c011b13eb5caedc3aa4541b5c5506b95d013cb7ba0f1cf66cf
+
+if [[ ! $(sha256sum script.deb.sh) == "${expected_sum}  script.deb.sh" ]]; then
+    echo Unexpected hash from git-lfs install script
+    exit 1
+fi
+
+# Install packagecloud's repository signing key and add repository to apt
+bash ./script.deb.sh
+
+set -xe
+
+# Install git-lfs
+apt-get install -y --no-install-recommends git-lfs
