25697f163e296a7f03a513d0140b2b31ac109029
Vinicius D. Cerutti <51954708+viniciusdc@users.noreply.github.com>
2024-04-18 11:51:18 -0300

    Update codeserver version to latest (#138)
    Update install-code-server.sh
    
    Update codeserver version from v4.5.1 to v4.23.1

--- scripts/install-code-server.sh
+++ scripts/install-code-server.sh
@@ -5,14 +5,14 @@
 set -xe
 DEFAULT_PREFIX="${1}"
 shift # path to environment yaml or lock file
-CODE_SERVER_VERSION=4.5.1
+CODE_SERVER_VERSION=4.23.1
 
 mkdir -p ${DEFAULT_PREFIX}/code-server
 cd ${DEFAULT_PREFIX}/code-server
 
 # Fetch the snapshot of https://code-server.dev/install.sh as of the time of writing
-wget --quiet https://raw.githubusercontent.com/coder/code-server/326a1d1862872955cec062030df2bd103799a1eb/install.sh
-expected_sum=ed18563871beb535130019b6c5b62206cc4a60c8bf4256aae96ce737951fc253
+wget --quiet https://raw.githubusercontent.com/coder/code-server/v4.23.1/install.sh
+expected_sum=ef0324043bc7493989764315e22bbc85c38c4e895549538b7e701948b64495e6
 
 if [[ ! $(sha256sum install.sh) == "${expected_sum}  install.sh" ]]; then
     echo Unexpected hash from code-server install script
