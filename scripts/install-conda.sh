680e08f8770c4f5dc61962da563e5e5861d8061b
Satrajit Ghosh <satrajit.ghosh@gmail.com>
2025-07-01 20:30:45 -0400

    Update install-conda.sh to use miniforge

--- scripts/install-conda.sh
+++ scripts/install-conda.sh
@@ -4,37 +4,37 @@
 
 set -xe
 
-# Requires environment MAMBAFORGE_SHA256, MINIFORGE_VERSION, and DEFAULT_ENV
+# Requires environment MINIFORGE_SHA256, MINIFORGE_VERSION, and DEFAULT_ENV
 arch=$(uname -i)
-wget --quiet -O mambaforge.sh https://github.com/conda-forge/miniforge/releases/download/$MAMBAFORGE_VERSION/Mambaforge-Linux-$arch.sh
+wget --quiet -O miniforge.sh https://github.com/conda-forge/miniforge/releases/download/$MINIFORGE_VERSION/Miniforge3-Linux-$arch.sh
 
 if [[ $arch == "aarch64" ]]; then
-  echo "${MAMBAFORGE_AARCH64_SHA256} mambaforge.sh" >mambaforge.checksum
+  echo "${MINIFORGE_AARCH64_SHA256} miniforge.sh" >miniforge.checksum
 elif [[ $arch == "x86_64" ]]; then
-  echo "${MAMBAFORGE_X86_64_SHA256} mambaforge.sh" >mambaforge.checksum
+  echo "${MINIFORGE_X86_64_SHA256} miniforge.sh" >miniforge.checksum
 else
   echo "Unsupported architecture: $arch"
   exit 1
 fi
 
-echo $(sha256sum -c mambaforge.checksum)
+echo $(sha256sum -c miniforge.checksum)
 
-if [ $(sha256sum -c mambaforge.checksum | awk '{print $2}') != "OK" ]; then
+if [ $(sha256sum -c miniforge.checksum | awk '{print $2}') != "OK" ]; then
   echo Error when testing checksum
   exit 1
 fi
 
-# Install Mamba and clean-up
+# Install Miniforge and clean-up
 if [ -d "/opt/conda" ]; then
-  sh ./mambaforge.sh -b -u -p /opt/conda
+  sh ./miniforge.sh -b -u -p /opt/conda
 else
-  sh ./mambaforge.sh -b -p /opt/conda
+  sh ./miniforge.sh -b -p /opt/conda
 fi
 
-rm mambaforge.sh mambaforge.checksum
+rm miniforge.sh miniforge.checksum
 
-mamba --version
-mamba clean -afy
+conda --version
+conda clean -afy
 
 ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
 
